fe12ae1a00fd4de0e216f0ec478d244b7ea54e8f
VladM <vlad.mishaev.1998@gmail.com>
2023-06-04 19:20:21 +0300

    add Cmake

---
+++ build/CMakeFiles/seflysend.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/seflysend.dir/src/login/sign/sign.cpp.o"
+  "CMakeFiles/seflysend.dir/src/login/sign/sign.cpp.o.d"
+  "CMakeFiles/seflysend.dir/src/login/sign_in/signIn.cpp.o"
+  "CMakeFiles/seflysend.dir/src/login/sign_in/signIn.cpp.o.d"
+  "CMakeFiles/seflysend.dir/src/login/sign_up/signUp.cpp.o"
+  "CMakeFiles/seflysend.dir/src/login/sign_up/signUp.cpp.o.d"
+  "CMakeFiles/seflysend.dir/src/main.cpp.o"
+  "CMakeFiles/seflysend.dir/src/main.cpp.o.d"
+  "seflysend"
+  "seflysend.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/seflysend.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/seflysend.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for seflysend.
+# This may be replaced when dependencies are built.
