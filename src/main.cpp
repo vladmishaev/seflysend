4ed4de070ba83fa853acfab25bbd931a41804f2e
VladM <vlad.mishaev.1998@gmail.com>
2023-06-07 17:54:01 +0300

    start to work on socket day-1

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,24 @@
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <iostream>
 
 int main()
 {
+    int sock, listener;
+    struct sockaddr_in addr;
+    char buf[1024];
+    int bytes_read;
+
+    listener = socket(AF_INET,SOCK_STREAM,0);
+    if(listener < 0)
+    {
+        perror("Socket Error: Can't create soket.");
+        exit(2);
+    }
     
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(3425);
+    addr.sin_addr.s_addr;
     return 0;
 }
